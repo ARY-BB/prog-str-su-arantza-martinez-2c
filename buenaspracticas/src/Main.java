1eee89883d1fd6d002f3e6c54ab464abc007eec5
ARY-BB <20253ds071@utez.edu.mx>
2026-01-17 20:36:36 -0600

    guardando el trabajo de java

---
+++ buenaspracticas/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    int limite = pedirEntero();
+    int sumaTotal = sumarHastaN(limite);
+
+    System.out.println("El resultado es: " + sumaTotal);
+  }
+
+  public static int pedirEntero() {
+    Scanner leer = new Scanner(System.in);
+    System.out.print("Dame un numero que sea entero: ");
+    return leer.nextInt();
+  }
+  /**
+
+   *
+   * @param n Se ocupa ese valor para saber hasta donde dejara de sumar
+   * @return regresa el valor de sumarHastaN
+   */
+  public static int sumarHastaN(int n) {
+    int total = 0;
+    for (int i = 1; i <= n; i++) {
+      total += i;
+    }
+    return total;
+  }
+}
