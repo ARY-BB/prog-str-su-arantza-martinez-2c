82b74d458ced198e5faec605cad18a7e39538716
ARY-BB <20253ds071@utez.edu.mx>
2026-01-20 17:25:15 -0600

    feat: agregar descuento por umbral

---
+++ ActividadRefactormartinezarantza/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Scanner leer=new Scanner(System.in);
+
+            System.out.print("a: ");
+            int a = scanner.nextInt();
+            System.out.print("b: ");
+            int b = scanner.nextInt();
+            System.out.print("c: ");
+            int c = scanner.nextInt();
+
+        int suma = a + b + c;
+        System.out.println("suma=" + suma);
+        System.out.println("prom=" + (suma / 3.0));
+    }
+    Scanner leer=new Scanner(System.in);
+    Int b,c;
+    public static pedirEntero(String[] args);
+    System.out.println("ingrese un numero entero: ");
+    Int a= leer.nextInt();
+
+
+}
--- MergeWithConflict/src/Main.java
+++ MergeWithConflict/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        system.out.println("mensaje final: main+ feature");
+        System.out.println("mensaje final con main y feature");
 
     }
 
